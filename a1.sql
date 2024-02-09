214231f399a6ef504133d8fce469847b319b3dc1
Lacunia <151394978+Lacunia@users.noreply.github.com>
2024-02-09 13:53:17 -0500

    Add files via upload

---
+++ a1.sql
@@ -0,0 +1,77 @@
+-- If you define any views for a question (you are encouraged to), you must drop them
+-- after you have populated the answer table for that question.
+-- Good Luck!
+
+-- Query 1b i --------------------------------------------------
+INSERT INTO Query1bi
+
+-- Query 1b ii --------------------------------------------------
+INSERT INTO Query1bii
+
+-- Query 1b iii --------------------------------------------------
+INSERT INTO Query1biii
+
+-- Query 1b iv  --------------------------------------------------
+INSERT INTO Query1biv
+
+-- Query 1b v --------------------------------------------------
+INSERT INTO Query1bv
+
+-- Query 1b vi --------------------------------------------------
+INSERT INTO Query1bvi
+
+-- Query 1b vii --------------------------------------------------
+INSERT INTO Query1bvii
+
+-- Query 1b ix --------------------------------------------------
+INSERT INTO Query1bix
+
+-- Query 1b x --------------------------------------------------
+INSERT INTO Query1bx
+
+-- Query 1c i --------------------------------------------------
+INSERT INTO Query1ci
+
+-- Query 1c ii --------------------------------------------------
+INSERT INTO Query1cii
+
+-- Query 1c iii --------------------------------------------------
+INSERT INTO Query1ciii
+
+-- Query 1c iv  --------------------------------------------------
+INSERT INTO Query1civ
+
+-- Query 2 i --------------------------------------------------
+INSERT INTO Query2i
+
+-- Query 2 ii --------------------------------------------------
+INSERT INTO Query2ii
+
+-- Query 2 iii --------------------------------------------------
+INSERT INTO Query2iii
+
+-- Query 2 iv  --------------------------------------------------
+INSERT INTO Query2iv
+
+-- Query 2 v --------------------------------------------------
+INSERT INTO Query2v
+
+-- Query 2 vi --------------------------------------------------
+INSERT INTO Query2vi
+
+-- Query 2 vii --------------------------------------------------
+INSERT INTO Query2vii
+
+-- Query 2 viii --------------------------------------------------
+INSERT INTO Query2viii
+
+
+
+
+
+
+
+
+
+
+
