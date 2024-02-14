c90b636fc48b5ff6c734ca9d9ce9a9fca10ebdfc
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-02-14 17:51:12 -0500

    Completed 2 iii, iv, v

--- a1.sql
+++ a1.sql
@@ -102,7 +102,7 @@ WHERE subid NOT IN (SELECT subid FROM Subsuppliers WHERE Subsuppliers.sid = reci
 UNION
 
 SELECT DISTINCT reciprocal1.sid1, reciprocal1.sid2, subid, subname, subaddress
-FROM (SELECT sid AS sid1, subid AS sid2 FROM Subsuppliers) AS reciprocal1
+FROM (SELECT sid AS sid1, subid AS sid2 FROM Subsuppliers) AS reciprocal16
 JOIN (SELECT sid AS sid1, subid AS sid2 FROM Subsuppliers) AS reciprocal2
 ON reciprocal1.sid1 = reciprocal2.sid2 AND reciprocal1.sid2 = reciprocal2.sid1 
 -- we get the subsuppliers for reciprocal supplier 2, where the subsuppliers are not the reciprocal supplier 1
@@ -134,20 +134,95 @@ ON Student.utorid = Approved1.utorid);
 INSERT INTO Query2ii
 -- Getting the rooms that are approved for employees
 SELECT utorid
+-- copy 1
 FROM (SELECT utorid AS utorid1, roomid AS roomid1 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved1 
+-- copy 2
 JOIN (SELECT utorid AS utorid2, roomid AS roomid2 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved2
+-- Making sure there are at least two rooms approved under the same utorid
 ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved2.roomid2
+-- copy 3
 JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved3
+-- Making sure there are at least three rooms approved under the same utorid
 ON Approved1.utorid1 = Approved3.utorid3 AND Approved1.roomid1 != Approved3.roomid3 AND Approved.roomid2 != Approved.roomid3;
 
 -- Query 2 iii --------------------------------------------------
 INSERT INTO Query2iii
+-- The idea is to subtract utorids with access to 4+ rooms from access to 3+ rooms to retain the ones with only access to exactly 3 rooms
+-- Getting the utorids with access to 3 or more rooms
+SELECT utorid
+-- copy 1
+FROM (SELECT utorid AS utorid1, roomid AS roomid1 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved1 
+-- copy 2
+JOIN (SELECT utorid AS utorid2, roomid AS roomid2 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved2
+-- Making sure there are at least two rooms approved under the same utorid
+ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved2.roomid2
+-- copy 3
+JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved3
+-- Making sure there are at least three rooms approved under the same utorid
+ON Approved1.utorid1 = Approved3.utorid3 AND Approved1.roomid1 != Approved3.roomid3 AND Approved.roomid2 != Approved.roomid3
+
+EXCEPT
+
+-- Getting the utorids with access to 4 or more rooms 
+(SELECT utorid
+-- copy 1
+FROM (SELECT utorid AS utorid1, roomid AS roomid1 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved1 
+-- copy 2
+JOIN (SELECT utorid AS utorid2, roomid AS roomid2 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved2
+-- Making sure there are at least two rooms approved under the same utorid
+ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved2.roomid2
+-- copy 3
+JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved3
+-- Making sure there are at least three rooms approved under the same utorid
+ON Approved1.utorid1 = Approved3.utorid3 AND Approved1.roomid1 != Approved3.roomid3 AND Approved.roomid2 != Approved.roomid3
+-- copy 4
+JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved4
+-- There are at least 4 different rooms under the same utorid
+ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved4.roomid4 AND Approved2.roomid2 != Approved4.roomid4 AND Approved3.roomid3 != Approved4.roomid4);
 
 -- Query 2 iv  --------------------------------------------------
 INSERT INTO Query2iv
+-- The idea is to subtract utorids with access to 4+ rooms from access to all rooms to retain the ones with only access to 3 or less rooms
+-- Getting the utorids with access to 3 or more rooms
+SELECT utorid
+-- copy 1
+FROM Employee JOIN Approved ON Employee.utorid = Approved.utorid
+
+EXCEPT
+
+-- Getting the utorids with access to 4 or more rooms 
+(SELECT utorid
+-- copy 1
+FROM (SELECT utorid AS utorid1, roomid AS roomid1 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved1 
+-- copy 2
+JOIN (SELECT utorid AS utorid2, roomid AS roomid2 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved2
+-- Making sure there are at least two rooms approved under the same utorid
+ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved2.roomid2
+-- copy 3
+JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved3
+-- Making sure there are at least three rooms approved under the same utorid
+ON Approved1.utorid1 = Approved3.utorid3 AND Approved1.roomid1 != Approved3.roomid3 AND Approved.roomid2 != Approved.roomid3
+-- copy 4
+JOIN (SELECT utorid AS utorid3, roomid AS roomid3 FROM (Employee JOIN Approved ON Employee.utorid = Approved.utorid)) AS Approved4
+-- There are at least 4 different rooms under the same utorid
+ON Approved1.utorid1 = Approved2.utorid2 AND Approved1.roomid1 != Approved4.roomid4 AND Approved2.roomid2 != Approved4.roomid4 AND Approved3.roomid3 != Approved4.roomid4);
 
 -- Query 2 v --------------------------------------------------
 INSERT INTO Query2v
+--In this case, we are accounting for all cases of when the alert level has exceeded the threshold, regardless of whether it was triggered by Oscar Lin or not
+-- getting the room id of where alert level has exceeded threshold between 2022-09-01 and 2022-12-31
+SELECT Room.roomid
+-- Checking Oscar Lin is indeed a student
+FROM (SELECT * FROM Student JOIN Member ON Student.utorid = Member.utorid AND Member.name = 'Oscar Lin') AS Student_O_L
+-- joining the approved table to find out all the rooms Oscar Lin was approved for
+JOIN Approved
+ON Student_O_L.utorid = Approved.utorid
+-- joining Occupancy table to find out all occurences of these rooms being occupied between 2022-09-01 and 2022-12-31
+JOIN Occupancy
+ON Approved.roomid = Occupancy.roomid AND (Occupancy.date BETWEEN 2022-09-01 AND 2022-12-31)
+-- joining Room table to find all occurences of when alert level is above the alert threshold level
+JOIN Room
+ON Approved.roomid = Room.roomid AND Occupancy.alertlevel > Room.alertthreshold;
 
 -- Query 2 vi --------------------------------------------------
 INSERT INTO Query2vi
